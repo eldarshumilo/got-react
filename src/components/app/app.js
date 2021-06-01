97bae0fdc56645ed99cc8ea7f7be9d2ea4ad3a37
Eldar <darik.shumilo@gmail.com>
2021-06-01 12:58:48 +0300

    mod 5

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,11 +2,36 @@ import React from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
-import ToggleButton from '../toggleButton/toggleButton';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import './toggleButton.css';
 
 
+class ToggleButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+      this.handleClick = this.handleClick.bind(this);
+      const viewForm = <RandomChar/>
+    }
+  
+    handleClick() {
+      this.setState(prevState => ({
+        isToggleOn: !prevState.isToggleOn
+      }));
+    }
+  
+    render() {
+      return (
+        <>
+            {this.state.isToggleOn ? this.viewForm = <RandomChar/> : null}
+            <button className='togButton' onClick={this.handleClick}>
+            {this.state.isToggleOn ? 'TOGGLE ON' : 'TOGGLE OFF'}
+            </button>
+        </>
+      );
+    }
+  }
 const App = () => {
     return (
         <> 
@@ -16,7 +41,7 @@ const App = () => {
             <Container>
                 <Row>
                     <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
+                        <viewForm/>
                     </Col>
                 </Row>
                 <Row>
